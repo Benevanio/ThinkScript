d20ac4667d0930892d3522326b13a03c9130b472
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-04-06 11:19:35 -0300

    Criando o label do sistema e aplicando classes no mesmo

--- src/main/java/thinscript/com/App.java
+++ src/main/java/thinscript/com/App.java
@@ -1,16 +1,36 @@
 package thinscript.com;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
 import javafx.stage.Stage;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
-
+    /*
+     * O primaryStage é a janela principal da aplicação
+     * ou seja ele é o container principal da aplicação
+     */
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Sistema da livraria com Java FX");
+        Group root = new Group();
+        Label label = new Label("Listagem de Produtos");
+        root.getChildren().add(label);
+        label.setFont(Font .font(
+            "Lucida Sans Unicode", FontPosture.REGULAR, 20
+        ));
+        label.setLayoutX(10);
+        label.setLayoutY(10);
+        label.setPadding(new Insets(10, 10, 10, 10));
+        Scene scene = new Scene(root, 940, 680);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Thinscript");
         primaryStage.show();
     }
 
